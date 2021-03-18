8bf62d497fb5011c195b99afdb594e0bcd981a34
Mark L Taylor <taylormarkld@gmail.com>
2021-03-17 20:22:21 -0700

    added routing for creating new donation

--- main.py
+++ main.py
@@ -3,19 +3,38 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
+
+import peewee
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        try:
+            donor = Donor.get(Donor.name == request.form['donor'])
+            donation = Donation(donor=donor, value=int(request.form['value']))
+            donation.save()
+
+            return redirect(url_for('all'))
+        except peewee.DoesNotExist as e:
+            return render_template('create.jinja2', error="Donor {} not in database".format(request.form['donor']))
+    else:
+        return render_template('create.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
